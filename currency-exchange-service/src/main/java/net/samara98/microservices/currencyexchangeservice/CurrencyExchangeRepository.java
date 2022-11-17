a450f43b219a898cc80a2fc20d73d54d666a126c
Sulthon Abdul Malik <sulthon.abdmalik@gmail.com>
2022-11-18 01:15:48 +0700

    Microservices with Spring Cloud

---
+++ currency-exchange-service/src/main/java/net/samara98/microservices/currencyexchangeservice/CurrencyExchangeRepository.java
@@ -0,0 +1,8 @@
+package net.samara98.microservices.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository
+        extends JpaRepository<CurrencyExchange, Long> {
+    CurrencyExchange findByFromAndTo(String from, String to);
+}
